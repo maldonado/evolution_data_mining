83768bde6095fb6f2474b76846f5020ffc895111
Everton Maldonado <everton.maldonado@gmail.com>
2015-04-01 20:15:50 -0400

    add functionality to remove invisible files from the copied folders

--- sonar.py
+++ sonar.py
@@ -1,11 +1,10 @@
+import os
 import sys
 import subprocess
-import os
+import shutil
 
 # get all tags
 tags = subprocess.check_output(["git", "tag"]).split()
-
-
 
 directory = subprocess.check_output(["pwd"]).split()[0]
 projectName = directory.split('/')[-1]
@@ -16,13 +15,38 @@ counter = 0
 for tag in tags:
     # checkout into each of them
     subprocess.call(["git", "checkout" , tag])
+    
     # create directory to place all the tags
-    subprocess.call(["mkdir", "../"+projectName+"_tags/"+tag])
-    # create sub directory src, to run sonar analysis
-    subprocess.call(["mkdir", "../"+projectName+"_tags/"+tag+"/src"])
+    folder_path = "../"+projectName+"_tags/"+tag
+    subprocess.call(["mkdir", folder_path])
 
+    # create sub directory src, to run sonar analysis
+
+    subprocess.call(["mkdir", folder_path+"/src"])
+
+    # copy checkout tag to its specific directory
+    subprocess.call(["cp", "-r",  "./" , folder_path+"/src"])
+
+    # walk the folder to remove unwanted files and folders (invisible files and third party) 
+    for root, dirs, files in os.walk(folder_path):
+        for f in files:
+            # removes invisible files
+            if f[0] == '.' :
+                os.unlink(os.path.join(root, f))
+            # if f == 'LICENSE' :
+            #     os.unlink(os.path.join(root, f))
+            # if f == 'run.sh':
+            #     os.unlink(os.path.join(root, f))
+        for d in dirs:
+            # remove invisible directories
+            if d[0] == '.': 
+                shutil.rmtree(os.path.join(root, d))
+            # if d == 'test' :
+            #     shutil.rmtree(os.path.join(root, d))
+                
+            
     # create sonar-project.properties file 
-    f = open("../"+projectName+"_tags/"+tag+"/sonar-project.properties", "wb")
+    f = open(folder_path + "/sonar-project.properties", "wb")
     f.write("# Required metadata\n")
     f.write("sonar.projectKey=" +projectName+ "\n")
     f.write("sonar.projectName=" +projectName+ "\n")
