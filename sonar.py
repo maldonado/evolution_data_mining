691169bc308d4d92155fc2226492a62f1431d037
Everton Maldonado <everton.maldonado@gmail.com>
2015-04-02 13:40:01 -0400

    add regex to match and delete invisible files, and unwanted folders like third party libs and examples that can misslead the results of the analysis

--- sonar.py
+++ sonar.py
@@ -1,17 +1,25 @@
 import os
+import re
 import sys
 import subprocess
 import shutil
 
-# get all tags
+# get all tags from the git repository
 tags = subprocess.check_output(["git", "tag"]).split()
 
+# get directory name and project name to create folders dynamically
 directory = subprocess.check_output(["pwd"]).split()[0]
 projectName = directory.split('/')[-1]
 
+# create folder to keep all the tags
 subprocess.call(["mkdir", "../"+projectName+"_tags"])
 
+#regex to eliminate folders that is not relevant for the analysis, like third party libraries and invisible files
+file_regex = '\..*' 
+directory_regex = '\..*|vendor|thirdparty|(:?ex|s)amples|doc(:?s|uments)|bin|node' 
+
 counter = 0
+
 for tag in tags:
     # checkout into each of them
     subprocess.call(["git", "checkout" , tag])
@@ -21,7 +29,6 @@ for tag in tags:
     subprocess.call(["mkdir", folder_path])
 
     # create sub directory src, to run sonar analysis
-
     subprocess.call(["mkdir", folder_path+"/src"])
 
     # copy checkout tag to its specific directory
@@ -31,20 +38,13 @@ for tag in tags:
     for root, dirs, files in os.walk(folder_path):
         for f in files:
             # removes invisible files
-            if f[0] == '.' :
+            if re.match(file_regex, f) is not None:
                 os.unlink(os.path.join(root, f))
-            # if f == 'LICENSE' :
-            #     os.unlink(os.path.join(root, f))
-            # if f == 'run.sh':
-            #     os.unlink(os.path.join(root, f))
         for d in dirs:
-            # remove invisible directories
-            if d[0] == '.': 
+            # regex to eliminate unwanted folders, like third party libraries and invisible files   
+            if re.match(directory_regex, d) is not None:
                 shutil.rmtree(os.path.join(root, d))
-            # if d == 'test' :
-            #     shutil.rmtree(os.path.join(root, d))
                 
-            
     # create sonar-project.properties file 
     f = open(folder_path + "/sonar-project.properties", "wb")
     f.write("# Required metadata\n")
